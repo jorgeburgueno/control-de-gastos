96c5c101c1ea42c6dd3798245265f86976802ec0
jorgeburgueno <jorge.albert16@hotmail.com>
2025-02-22 00:14:58 -0700

    agregado eliminacion de gastos

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let main = document.querySelector(".main");
+let gastosContainer = document.querySelector(".gastos-container");
 let gastoForm = document.querySelector("#gastos-form")
 let btnNuevo = document.querySelector('.nuevo-btn');
-let btnDelete = document.querySelector('#eliminar');
+let btnDelete = document.querySelector('.eliminar');
 
 
 let gastos = [];
@@ -26,27 +27,44 @@ gastoForm.addEventListener('submit', (event) => {
 
 function addGasto(gasto){
     gastos.push(gasto);
-    render(gasto);
+    render(gasto, gastos.length - 1);
 };
 
 //borra un gasto de la pantalla y del array
 
+gastosContainer.addEventListener('click', (event) =>{
+    if (event.target.classList.contains('eliminar')) {
+        const gastoDiv = event.target.closest('.gasto');
+        const gastoId = gastoDiv.getAttribute('data-id');
 
+        gastoDiv.remove();
+        gastos.splice(gastoId, 1);
+        console.log('Expense deleted:', gastos);
+
+        renderAllGastos();
+    }
+})
 
 //hace que el gasto aparezca en pantalla 
 
-function render(gasto){
+function render(gasto, index){
     const nuevoGasto = document.createElement('div');
-    nuevoGasto.classList.add('gastos');
+    nuevoGasto.classList.add('gasto');
+    nuevoGasto.setAttribute('data-id', index);
     nuevoGasto.innerHTML = `<p> categoria: ${gasto.categoria}</p>
                             <p> cantidad: ${gasto.cantidad}</p>
                             <p> fecha: ${gasto.fecha}</p>
-                            <button id= 'eliminar'>eliminar</button> 
+                            <button class= 'eliminar'>eliminar</button> 
                             `
-    main.appendChild(nuevoGasto);
+    gastosContainer.appendChild(nuevoGasto);
 }
 gastos.forEach(gasto => {
     render(gasto)
 });
 
-
+function renderAllGastos(){
+    gastosContainer.innerHTML = '';
+    gastos.forEach((gasto, index) => {
+        render(gasto, index)
+    });
+}
