291ed38b279fdab1a4d7a64f9c63dc5f06a1a05a
jorgeburgueno <jorge.albert16@hotmail.com>
2025-02-21 23:08:32 -0700

    añadido creacion de gastos y se muestran correctamente en pantalla

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 let main = document.querySelector(".main");
+let gastoForm = document.querySelector("#gastos-form")
+let btnNuevo = document.querySelector('.nuevo-btn');
+let btnDelete = document.querySelector('#eliminar');
 
-let gastos = [
-    {categoria: "renta", cantidad: 500, fecha: "01/01/2025"},
-    {categoria: "gas", cantidad: 100, fecha: "02/01/2025"}
-];
+
+let gastos = [];
+
+//crear gasto nuevo
+btnNuevo.addEventListener('click', () => {
+    gastoForm.classList.remove('hidden');
+});
+
+gastoForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    let gasto = {
+        categoria: document.querySelector('#categoria').value,
+        cantidad: document.querySelector('#cantidad').value,
+        fecha: document.querySelector('#fecha').value
+    };
+    console.log(gastos);
+    addGasto(gasto);
+    gastoForm.classList.add('hidden');
+});
+
+function addGasto(gasto){
+    gastos.push(gasto);
+    render(gasto);
+};
+
+//borra un gasto de la pantalla y del array
+
+
+
+//hace que el gasto aparezca en pantalla 
 
 function render(gasto){
     const nuevoGasto = document.createElement('div');
@@ -11,9 +41,12 @@ function render(gasto){
     nuevoGasto.innerHTML = `<p> categoria: ${gasto.categoria}</p>
                             <p> cantidad: ${gasto.cantidad}</p>
                             <p> fecha: ${gasto.fecha}</p>
+                            <button id= 'eliminar'>eliminar</button> 
                             `
     main.appendChild(nuevoGasto);
 }
 gastos.forEach(gasto => {
     render(gasto)
 });
+
+
